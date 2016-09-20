43fb6fa5ef69e6e34592018139879feceed42deb
Eleazar Arroyo <eleazar.a@htk-id.com>
2016-09-20 12:07:43 -0500

    First commit
    Adding HandheldDetector folder

---
+++ HandheldDetector_wf/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace HandheldDetector_wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lvFiles = new System.Windows.Forms.ListView();
+            this.cFiles2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lvFolders = new System.Windows.Forms.ListView();
+            this.cFiles1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btCopy = new System.Windows.Forms.Button();
+            this.btUpload = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.tbDB = new System.Windows.Forms.TextBox();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(491, 285);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel3);
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Controls.Add(this.panel4);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(483, 259);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Handheld";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.tbPwd);
+            this.tabPage2.Controls.Add(this.tbUser);
+            this.tabPage2.Controls.Add(this.tbDB);
+            this.tabPage2.Controls.Add(this.tbHost);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(483, 259);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Configuración";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lvFiles);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(163, 34);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(317, 186);
+            this.panel3.TabIndex = 7;
+            // 
+            // lvFiles
+            // 
+            this.lvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cFiles2,
+            this.cDate});
+            this.lvFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvFiles.FullRowSelect = true;
+            this.lvFiles.HideSelection = false;
+            this.lvFiles.Location = new System.Drawing.Point(0, 0);
+            this.lvFiles.Name = "lvFiles";
+            this.lvFiles.Size = new System.Drawing.Size(317, 186);
+            this.lvFiles.TabIndex = 0;
+            this.lvFiles.UseCompatibleStateImageBehavior = false;
+            this.lvFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // cFiles2
+            // 
+            this.cFiles2.Text = "Archivos";
+            this.cFiles2.Width = 150;
+            // 
+            // cDate
+            // 
+            this.cDate.Text = "Fecha";
+            this.cDate.Width = 150;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lvFolders);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(3, 34);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(160, 186);
+            this.panel2.TabIndex = 6;
+            // 
+            // lvFolders
+            // 
+            this.lvFolders.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cFiles1});
+            this.lvFolders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvFolders.FullRowSelect = true;
+            this.lvFolders.HideSelection = false;
+            this.lvFolders.Location = new System.Drawing.Point(0, 0);
+            this.lvFolders.Name = "lvFolders";
+            this.lvFolders.Size = new System.Drawing.Size(160, 186);
+            this.lvFolders.TabIndex = 0;
+            this.lvFolders.UseCompatibleStateImageBehavior = false;
+            this.lvFolders.View = System.Windows.Forms.View.Details;
+            this.lvFolders.SelectedIndexChanged += new System.EventHandler(this.lvFolders_SelectedIndexChanged);
+            // 
+            // cFiles1
+            // 
+            this.cFiles1.Text = "Carpetas";
+            this.cFiles1.Width = 150;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btCopy);
+            this.panel4.Controls.Add(this.btUpload);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(3, 220);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(477, 36);
+            this.panel4.TabIndex = 8;
+            // 
+            // btCopy
+            // 
+            this.btCopy.Location = new System.Drawing.Point(305, 7);
+            this.btCopy.Name = "btCopy";
+            this.btCopy.Size = new System.Drawing.Size(75, 23);
+            this.btCopy.TabIndex = 1;
+            this.btCopy.Text = "Copiar";
+            this.btCopy.UseVisualStyleBackColor = true;
+            this.btCopy.Click += new System.EventHandler(this.btCopy_Click);
+            // 
+            // btUpload
+            // 
+            this.btUpload.Location = new System.Drawing.Point(386, 7);
+            this.btUpload.Name = "btUpload";
+            this.btUpload.Size = new System.Drawing.Size(75, 23);
+            this.btUpload.TabIndex = 0;
+            this.btUpload.Text = "Subir";
+            this.btUpload.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.statusLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(477, 31);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Conexión:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(157, 9);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(51, 13);
+            this.statusLabel.TabIndex = 0;
+            this.statusLabel.Text = "Conexión";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(154, 79);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(252, 20);
+            this.tbHost.TabIndex = 0;
+            this.tbHost.Text = "192.168.25.123";
+            // 
+            // tbDB
+            // 
+            this.tbDB.Location = new System.Drawing.Point(154, 106);
+            this.tbDB.Name = "tbDB";
+            this.tbDB.Size = new System.Drawing.Size(252, 20);
+            this.tbDB.TabIndex = 1;
+            this.tbDB.Text = "autobuildHTK-doihiFZ";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(154, 133);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(252, 20);
+            this.tbUser.TabIndex = 2;
+            this.tbUser.Text = "IosUser3";
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Location = new System.Drawing.Point(154, 160);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.Size = new System.Drawing.Size(252, 20);
+            this.tbPwd.TabIndex = 3;
+            this.tbPwd.Text = "IU2015!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Host:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Base de datos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Usuario:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(75, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Contraseña:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 285);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AssetsApp Uploader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListView lvFiles;
+        private System.Windows.Forms.ColumnHeader cFiles2;
+        private System.Windows.Forms.ColumnHeader cDate;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView lvFolders;
+        private System.Windows.Forms.ColumnHeader cFiles1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btCopy;
+        private System.Windows.Forms.Button btUpload;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPwd;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbDB;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
